c3438dde32b7a48299fe3b162c2812790a70075d
Vasiliy <kkolyasev1228@gmail.com>
2020-06-26 10:04:06 +0300

    asside

--- single.php
+++ single.php
@@ -10,30 +10,23 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'clean' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'clean' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+    <div id="fh5co-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="row">
+                        <div class="col-md-3 col-md-push-9 animate-box">
+                            <? get_sidebar(); ?>
+                        </div>
+                        <div class="col-md-9 col-md-pull-3">
+                            <? get_template_part('template-parts/content',
+                            get_post_format()); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php
 get_sidebar();
